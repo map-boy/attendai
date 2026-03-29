3516611ef4e1780c8d439d1d200f6054f2a7a748
map-boy <umwemubi@gmail.com>
2026-03-29 21:18:30 +0200

    fix: vite build only

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import StudentForm from './pages/StudentForm'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
         <Route path="/" element={<TeacherDashboard />} />
         <Route path="/student" element={<StudentForm />} />
